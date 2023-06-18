af69b661a66826f81d50940512fa8f4348bea0f7
UmmarHisham <88609198+UmmarHisham@users.noreply.github.com>
2023-06-18 23:14:06 +0800

    Add files via upload

---
+++ Part 2/Card.java
@@ -0,0 +1,52 @@
+public class Card {
+    String suit; 
+    String rank;
+
+    Card(){}
+
+    // Initialize the Card
+    Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public String getRank() {
+        return this.rank;
+    }
+
+    // Set value to cards based on rank
+    public int setValue() {
+        if (rank.equals("A")) {
+            return 14;
+        } else if (rank.equals("J") ){
+            return 11;
+        } else if (rank.equals("Q") ){
+            return 12;
+        }else if (rank.equals("K") ){
+            return 13;
+        }else {
+            return Integer.parseInt(rank);    
+        }
+    }    
+
+    public boolean isEqual(Card card) {
+        if (this.suit.equals(card.suit) && this.rank.equals(card.rank))
+            return true;
+        else
+            return false;
+    }
+
+    
+    public String getCard() {
+        return this.suit + this.rank;
+    }
+
+    public String toString(){
+        return this.suit + this.rank;
+    }
+
+}
